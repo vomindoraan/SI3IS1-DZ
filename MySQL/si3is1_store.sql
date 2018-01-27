f6b0ae1d308dd03afc44672ed06bad67cdf700b7
Konstantin Đorđević <vomindoraan@gmail.com>
2018-01-27 21:02:18 +0100

    Add database dumps

---
+++ MySQL/si3is1_store.sql
@@ -0,0 +1,223 @@
+CREATE DATABASE  IF NOT EXISTS `si3is1_store` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `si3is1_store`;
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: si3is1_store
+-- ------------------------------------------------------
+-- Server version	5.7.21-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employee`
+--
+
+DROP TABLE IF EXISTS `employee`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employee` (
+  `id_employee` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `id_store` int(11) NOT NULL,
+  PRIMARY KEY (`id_employee`),
+  KEY `fk_id_store_employee_idx` (`id_store`),
+  CONSTRAINT `fk_id_store_employee` FOREIGN KEY (`id_store`) REFERENCES `store` (`id_store`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employee`
+--
+
+LOCK TABLES `employee` WRITE;
+/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
+INSERT INTO `employee` VALUES (1,'Janko Đurić',1),(2,'Milanka Radovanović',3),(3,'Aleksa Bolo',1),(4,'Božidar Marketić',2),(5,'Dragan Jović',2),(6,'Jovica Pršo',1),(7,'Vladislava Ivić',3),(8,'Periša Đorđević',1),(9,'Marija Šapić',1),(10,'Jovana Markoš',2),(11,'Kristina Belomarković-Popkonstantinović',2);
+/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventory`
+--
+
+DROP TABLE IF EXISTS `inventory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `inventory` (
+  `id_store` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL,
+  `amount` float NOT NULL,
+  PRIMARY KEY (`id_store`,`id_product`),
+  KEY `fk_id_product_inventory_idx` (`id_product`),
+  KEY `fk_id_store_inventory_idx` (`id_store`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventory`
+--
+
+LOCK TABLES `inventory` WRITE;
+/*!40000 ALTER TABLE `inventory` DISABLE KEYS */;
+INSERT INTO `inventory` VALUES (1,1,11),(1,3,10),(1,5,7.5),(1,6,10),(2,1,10),(2,2,5),(2,3,1),(2,4,10),(2,6,8.02),(3,2,10),(3,3,10);
+/*!40000 ALTER TABLE `inventory` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `manager`
+--
+
+DROP TABLE IF EXISTS `manager`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `manager` (
+  `id_manager` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`id_manager`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `manager`
+--
+
+LOCK TABLES `manager` WRITE;
+/*!40000 ALTER TABLE `manager` DISABLE KEYS */;
+INSERT INTO `manager` VALUES (1,'Dragan Jošić'),(2,'Vatroslav Šota'),(3,'Marina Kralj'),(4,'NN lice');
+/*!40000 ALTER TABLE `manager` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `product` (
+  `id_product` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `price` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id_product`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'white bread','bread',25.00),(2,'rye bread','bread',29.99),(3,'Moja kravica','yoghurt',39.99),(4,'Medeno srce','sweets',140.99),(5,'Staedtler','pencil',28.45),(6,'A4 plain','paper',55.89);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reservation`
+--
+
+DROP TABLE IF EXISTS `reservation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `reservation` (
+  `id_reservation` int(11) NOT NULL AUTO_INCREMENT,
+  `id_store` int(11) NOT NULL,
+  `id_product` int(11) NOT NULL,
+  `customer_name` varchar(45) NOT NULL,
+  `customer_phone_no` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id_reservation`),
+  KEY `fk_id_store_reservation_idx` (`id_store`),
+  KEY `fk_id_product_reservation_idx` (`id_product`),
+  CONSTRAINT `fk_id_product_reservation` FOREIGN KEY (`id_product`) REFERENCES `product` (`id_product`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id_store_reservation` FOREIGN KEY (`id_store`) REFERENCES `store` (`id_store`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservation`
+--
+
+LOCK TABLES `reservation` WRITE;
+/*!40000 ALTER TABLE `reservation` DISABLE KEYS */;
+INSERT INTO `reservation` VALUES (2,3,3,'Jova',NULL);
+/*!40000 ALTER TABLE `reservation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `store`
+--
+
+DROP TABLE IF EXISTS `store`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `store` (
+  `id_store` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `address` varchar(45) DEFAULT NULL,
+  `id_manager` int(11) NOT NULL,
+  PRIMARY KEY (`id_store`),
+  KEY `fk_id_manager_store_idx` (`id_manager`),
+  CONSTRAINT `fk_id_manager_store` FOREIGN KEY (`id_manager`) REFERENCES `manager` (`id_manager`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `store`
+--
+
+LOCK TABLES `store` WRITE;
+/*!40000 ALTER TABLE `store` DISABLE KEYS */;
+INSERT INTO `store` VALUES (1,'Metro','Ibarski put 20',1),(2,'Maxi','Trgovačka 8a',2),(3,'Trlić','Karađorđeva 5',3);
+/*!40000 ALTER TABLE `store` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `turnover`
+--
+
+DROP TABLE IF EXISTS `turnover`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `turnover` (
+  `id_turnover` int(11) NOT NULL AUTO_INCREMENT,
+  `id_store` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `amount` float NOT NULL,
+  `profit` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id_turnover`),
+  UNIQUE KEY `id_store_date_turnover_uq` (`id_store`,`date`),
+  KEY `fk_id_store_turnover_idx` (`id_store`),
+  KEY `date_turnover_idx` (`date`),
+  CONSTRAINT `fk_id_store_turnover` FOREIGN KEY (`id_store`) REFERENCES `store` (`id_store`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `turnover`
+--
+
+LOCK TABLES `turnover` WRITE;
+/*!40000 ALTER TABLE `turnover` DISABLE KEYS */;
+INSERT INTO `turnover` VALUES (1,2,'2018-01-27',3.5,173.36),(2,1,'2018-01-27',0,0.00);
+/*!40000 ALTER TABLE `turnover` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-01-27 16:45:52
